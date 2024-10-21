82a1949f3054f046904e3c70505efd65267e72e4
React-Props <daniel.linker@intellicode-solutions.de>
2024-10-21 12:21:59 +0200

    routing user-list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,6 @@ import {UsersListComponent} from "./shared-ui/users-list/users-list.component";
 import {AppComponent} from "./app.component";
 
 export const routes: Routes = [
+  {path: '', redirectTo: 'user-list', pathMatch: "full"},
   {path: 'user-list', component: UsersListComponent}
 ];
