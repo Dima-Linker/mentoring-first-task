00df74c2cb73ce18c37a2a434381f78f053c245b
React-Props <daniel.linker@intellicode-solutions.de>
2024-10-21 10:26:12 +0200

    shared-ui

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import {UsersListComponent} from "./shared-ui/users-list/users-list.component";
+import {AppComponent} from "./app.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'user', component: UsersListComponent}
+];
