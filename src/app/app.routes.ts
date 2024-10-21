8d6b7a0d241272fceeacb3b1d65ead04607e6577
React-Props <daniel.linker@intellicode-solutions.de>
2024-10-21 13:46:06 +0200

    interfaces

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import {UsersListComponent} from "./shared-ui/users-list/users-list.component";
-import {AppComponent} from "./app.component";
 
 export const routes: Routes = [
   {path: '', redirectTo: 'user-list', pathMatch: "full"},
--- src/app/shared-ui/users-list/users-list.component.ts
+++ src/app/shared-ui/users-list/users-list.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import {RouterOutlet} from "@angular/router";
+import {MatCard, MatCardContent, MatCardHeader, MatCardSubtitle, MatCardTitle} from "@angular/material/card";
 
 @Component({
   selector: 'app-users-list',
   standalone: true,
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    MatCardHeader,
+    MatCard,
+    MatCardContent,
+    MatCardTitle,
+    MatCardSubtitle
   ],
   templateUrl: './users-list.component.html',
   styleUrl: './users-list.component.scss'
